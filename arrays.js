b5b541a86eb562dd8260eab9702c13502f9feab7
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-18 17:52:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,6 +29,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
    return array;
 }
 function removeElementFromEndOfArray(array){
-  array.slice(1,array.length - 1);
+  return array.slice();
 
 }
